be873eec188470971eaff352c5c60f044dbaca54
Celine Broomhead <celine.broomhead@gmail.com>
2016-07-05 17:46:43 +0000

    This a commit for the second excercise

--- answers.js
+++ answers.js
@@ -5,3 +5,13 @@ var firstLetter = function (string) {
 }
 firstLetter("alphabet");
 
+//Write a function that takes a string and returns the last character of a string. Test your function on a few inputs, including the empty string.
+
+var lastLetter = function (string) { 
+    for (i = 0; i < string.length; i++){
+        var letter = string.length - 1;
+      console.log(string[letter]);
+      }   
+};
+lastLetter("pizzazz");
+
