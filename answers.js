eaa3b14b55b1c8aa9b08f095cff1819741856554
Celine Broomhead <celine.broomhead@gmail.com>
2016-07-05 18:14:32 +0000

    START LOOKING AT THE COMMIT HERE PLEASE This commit has excercises 1-4

--- answers.js
+++ answers.js
@@ -17,3 +17,23 @@ var lastLetter = function (string) {
 };
 lastLetter("pizzazz");
 
+//Write a function that takes a string and a number, and returns the character at the position represented by the number. 
+//The indexing of number should start at 0. Test your function on a few inputs, including the empty string.
+
+var stringNumber = function (string, number) {
+    return string[number];
+};
+console.log(stringNumber("alphabet", 3));
+
+//Write a function that takes two numbers and adds them together. 
+//Test your function on a few inputs. Write in the comments what happens when you pass something other than a number to your function.
+
+var addNumbers = function (num1, num2){
+    return (num1 + num2);
+};
+console.log(addNumbers(1, 3)); // when i do not input a number, it gitve me a reference error that the non-number if undefined
+
+
+
+
+
