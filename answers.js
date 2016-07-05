87e46981d866112634b21336e86ffae4d27d3364
Celine Broomhead <celine.broomhead@gmail.com>
2016-07-05 17:45:15 +0000

    This is the commit for the first excercise

--- answers.js
+++ answers.js
@@ -1,1 +1,7 @@
-//answers
+//Write a function that takes a string and returns the first character of the string. Test your function on a few inputs, including the empty string.
+
+var firstLetter = function (string) {
+    return string.substring(0,1);
+}
+firstLetter("alphabet");
+
